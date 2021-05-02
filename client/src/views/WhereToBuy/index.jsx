44560bd321bfc24bbaaef26a9b05f2d6347165c0
Shellyda <shellydabarbosa@gmail.com>
2021-05-02 15:41:31 -0300

    feature(feature/comparation-feature): creating the bottons

--- client/src/views/WhereToBuy/index.jsx
+++ client/src/views/WhereToBuy/index.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { ProductModal } from '../../components';
-import { ChoiceIsYour, Container, RecommendBrand } from './styles';
+import { ChoiceIsYour, Container, RecommendBrand, ImgIconContainer, Title, Description, ContainerIconBottom } from './styles';
+import Icon from '../../components/Assets/Vector.svg';
 
 function WhereToBuy() {
     return (
+        <>
         <Container>
+            <ImgIconContainer href="#">
+                <img src={Icon} />
+            </ImgIconContainer>
             <ChoiceIsYour>
+                <Title>onde comprar</Title>
+                <Description>a escolha é sua!</Description>
                 <ProductModal />
                 <ProductModal />
             </ChoiceIsYour>
             <RecommendBrand>
+                <Title className='recommend'>recomendados</Title>
+                <Description className='recommend'>produtinhos livres de taxa rosa para você :)</Description>
                 <ProductModal />
                 <ProductModal />
             </RecommendBrand>
+
         </Container>
+    <ContainerIconBottom>
+    <ImgIconContainer href="#" className='bottom-icon'>
+         <img src={Icon} />
+     </ImgIconContainer>
+    </ContainerIconBottom>
+
+     </>
     )
 }
 
--- client/src/views/WhereToBuy/styles.js
+++ client/src/views/WhereToBuy/styles.js
@@ -4,7 +4,30 @@ export const Container = styled.div`
    display: flex;
 `;
 
+export const ImgIconContainer = styled.a`
+Width: 140px;
+Height: 70px;
+left: 46%;
+padding: 45px;
+margin: -156px 0px;
+border-top-left-radius: 60px;
+border-top-right-radius: 60px;
+background: white;
+position: absolute;
+align-items: center;
+display: flex;
+padding: 45px;
+cursor: pointer;
 
+&.bottom-icon{
+    transform: rotate(179deg);
+    border-bottom-left-radius: 60px;
+    border-bottom-right-radius: 60px;
+    border-top-left-radius: 0px;
+border-top-right-radius: 0px;
+    margin: 0px;
+}
+`;
 export const ChoiceIsYour = styled.div`
 display: flex;
 flex-direction: column;
@@ -15,10 +38,38 @@ height: auto;
 
 `;
 
+export const ContainerIconBottom = styled.div`
+    width: 50vw;
+    background-color: #E7475A;
+    align-items: center;
+    height: 90px;
+`;
+
 export const RecommendBrand = styled.div`
 display: flex;
 flex-direction: column;
 height: auto;
 width: 48.9vw;
 align-items: center;
+`;
+
+export const Title = styled.h2`
+font-style: normal;
+font-weight: normal;
+font-size: 64px;
+line-height: 2px;
+color: #FFFBE9;
+&.recommend{
+color: #E7475A;
+}
+`;
+
+export const Description = styled.p`
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+color: #FFFBE9;
+&.recommend{
+color: #E7475A;
+}
 `;
