d3dfaea82cc3ac65693e1c6c789fa01298cc1d7c
sofiadinizms <sdms@cin.ufpe.br>
2021-05-02 21:42:05 -0300

    feat(feature/price-comparison): finished footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Insta from '../Assets/instagram.png';
+import Facebook from '../Assets/facebook.png';
+import LinkedIn from '../Assets/linkedin.png';
 import{
   Container,
-  MangaRosa
+  MangaRosa,
+  SocialMedia,
+  SocialContainer
 } from './Styles';
 import Logo from '../Assets/logo.png';
 
@@ -9,6 +14,11 @@ function Footer(){
   return(
     <Container>
       <MangaRosa src={Logo}/>
+      <SocialContainer>
+        <SocialMedia src={LinkedIn} alt='LinkedIn do Manga Rosa'/>
+        <a href='https://www.instagram.com/desrosese/'><SocialMedia src={Insta} alt='Instagram do Manga Rosa' /></a>
+        <SocialMedia src={Facebook} alt='Facebook do Manga Rosa' />
+      </SocialContainer>
     </Container>
   )
 }
--- client/src/components/Footer/Styles.js
+++ client/src/components/Footer/Styles.js
@@ -6,14 +6,28 @@ export const Container = styled.section`
   width: 100%;
   background-color: ${Theme.colors.lightYellow};
   display: flex;
-  flex-direction: row wrap;
-  align-content: space-around;
+  flex-flow: row wrap;
+  align-content: center;
 `
 
 export const MangaRosa = styled.img`
   width: 100%;
   height: 36%;
   object-fit: contain;
+`
+
+export const SocialMedia = styled.img`
+  width: 40px;
+  height: 100%;
+  object-fit: contain;
+`
+
+export const SocialContainer = styled.div`
+  width: 100%;
+  height: 8%;
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
 `
 
 
